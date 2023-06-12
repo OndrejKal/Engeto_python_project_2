a2176c8b47400d9c44c1bd8ec68fdf8d70d77fd8
Ondrej Kalvas <kalvasondrej@gmail.com>
2023-06-13 01:51:12 +0200

    Bulls & Cows

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Ondřej Kalvas
+email: kalvasondrej@gmail.com
+discord: Ondřej K.#0612
+"""
+
+print("Hi there!")
+print("-" * 48)
